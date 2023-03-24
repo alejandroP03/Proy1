aba66b7bd65f206878137d13633292b2ba002044
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-03-23 23:07:22 -0500

    Modified fare and roofare logic

---
+++ Proyecto1/App/src/Model/HotelObjects/RoomRelated/Fare.java
@@ -0,0 +1,11 @@
+package Model.HotelObjects.RoomRelated;
+
+import org.json.simple.JSONObject;
+
+import Model.HotelObjects.HotelObject;
+
+public class Fare implements HotelObject{
+    public JSONObject getJsonObject() {
+        return null;
+    }
+}
