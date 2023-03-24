3dac439b6969e5b1ad21101aa7bb3254ac0df3a5
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-03-23 21:59:34 -0500

    Added faresRoomByType in Hotel

--- Proyecto1/App/src/Controller/Hotel.java
+++ Proyecto1/App/src/Controller/Hotel.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import java.util.Map;
+
+import Model.HotelObjects.HotelObject;
+
 public class Hotel {
+    private Map<String, HotelObject> roomFares;
+
+    public Map<String, HotelObject> getRoomFares() {
+        return roomFares;
+    }
 }
