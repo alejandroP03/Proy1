1801b46f4e471ca2f8dcc8f799e340a3e3959f66
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-03-28 18:12:53 -0500

    Created some Hotel methods

--- Proyecto1/App/src/Controller/Hotel.java
+++ Proyecto1/App/src/Controller/Hotel.java
@@ -1,70 +1,87 @@
 package Controller;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import Model.HotelDataHolder.*;
-import Model.HotelObjects.*;
+import Model.HotelDataHolder.BookingsDataHandler;
+import Model.HotelDataHolder.FaresDataHandler;
+import Model.HotelDataHolder.FoodDataHandler;
+import Model.HotelDataHolder.RegistrationDataHandler;
+import Model.HotelDataHolder.RoomsDataHandler;
+import Model.HotelDataHolder.ServicesDataHandler;
 import Model.HotelObjects.RoomRelated.Room;
-import Model.HotelObjects.RoomRelated.RoomFares;
 
 public class Hotel {
     // atributos
-
-    private HashMap<String, Room> roomList;
-    private HashMap<String, HotelObject> roomFares;
-
-    private HashMap<String, Service> services;
 
     private RoomsDataHandler roomsHandler = new RoomsDataHandler(new File("App/data/rooms.json"));
     private FaresDataHandler faresHandler = new FaresDataHandler(new File("App/data/room_fares.json"));
     private ServicesDataHandler servicesHandler = new ServicesDataHandler(new File("App/data/services.json"));
     private FoodDataHandler restaurantHandler = new FoodDataHandler(new File("App/data/foodInfo.json"));
-    //private BookingsDataHandler bookingsHandler = new BookingsDataHandler(new File("App/data/bookings.json"));
-    private RegistrationDataHandler registrationHandler = new RegistrationDataHandler(new File("App/data/registrations.json"));
-
-    private Map<Object,Room> roomsList;
-    private Map<Object,RoomFares> roomFaresList;
-    private Map<Object,Service> servicesList;
-    private Map<Object,Food> foodList;
-    private Map<Object, Booking> bookingList;
-    private Map<Object, Registration> registrationList;
-
+    private BookingsDataHandler bookingsHandler = new BookingsDataHandler(new File("App/data/bookings.json"));
+    private RegistrationDataHandler registrationHandler = new RegistrationDataHandler(
+            new File("App/data/registrations.json"));
 
     // metodos
 
     public RoomsDataHandler getRoomsHandler() {
-       return this.roomsHandler;
-   }
+        return this.roomsHandler;
+    }
 
     public ServicesDataHandler getServices() {
         return this.servicesHandler;
     }
 
-
     public FoodDataHandler getRestaurantHandler() {
         return this.restaurantHandler;
     }
 
+    public FaresDataHandler getFaresHandler() {
+        return faresHandler;
+    }
 
+    private ArrayList<Room> getRoomsWithCapacity(int capacity) {
 
+        ArrayList<Room> roomWithCapacity = new ArrayList<Room>();
+        for (Room room : this.roomsHandler.getData().values()) {
+            if (room.getCapacity() >= capacity)
+                roomWithCapacity.add(room);
+        }
 
-//
-//    public FaresDataHandler getFaresHandler() {
-//        return this.faresHandler;
-//    }
-//
-//    public ServicesDataHandler getServicesHandler() {
-//        return this.servicesHandler;
-//    }
+        return roomWithCapacity;
+    }
 
-//
-//    public BookingsDataHandler getBookingsHandler() {
-//        return this.bookingsHandler;
-//    }
-//
-//    public RegistrationDataHandler getRegistrationHandler() {
-//        return this.registrationHandler;
-//    }
+    public Map<String, Room> getFreeRooms(int capacity) {
+        Map<String, Room> freeRooms = new HashMap<String, Room>();
+
+        for (Room room : getRoomsWithCapacity(capacity)) {
+            if (!room.getIsOcupied())
+                freeRooms.put(room.getRoomId(), room);
+        }
+
+        return freeRooms;
+    }
+
+    public Map<String, Room> getNotBookedRooms(LocalDate initialDate, LocalDate finalDate, int capacity) {
+        Map<String, Room> freeRooms = new HashMap<String, Room>();
+        for (Room room : getRoomsWithCapacity(capacity)) {
+            for (Map.Entry<LocalDate, LocalDate> roomBookedDates : room.getBookedDates().entrySet()) {
+                LocalDate bookedInitialDate = roomBookedDates.getKey();
+                LocalDate bookedFinalDate = roomBookedDates.getValue();
+                // initialDate empieza antes de que bookedFinalDate termine o finaldate termina
+                // despues de que bookedInitialDate empiece
+                if (initialDate.compareTo(bookedFinalDate) <= 0)
+                    break;
+                if (finalDate.compareTo(bookedInitialDate) >= 0)
+                    break;
+                else
+                    freeRooms.put(room.getRoomId(), room);
+            }
+        }
+
+        return freeRooms;
+    }
 }
