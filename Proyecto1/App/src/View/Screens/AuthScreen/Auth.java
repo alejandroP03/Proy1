4ce5a15135528e569803fe5b1505c745c411a85a
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-04-30 20:48:18 -0500

    Modified styles of input selector

--- Proyecto1/App/src/View/Screens/AuthScreen/Auth.java
+++ Proyecto1/App/src/View/Screens/AuthScreen/Auth.java
@@ -1,12 +1,13 @@
 package View.Screens.AuthScreen;
 
-import View.Components.InputText;
+import View.Components.SelectorInput;
 import javafx.scene.layout.Pane;
 
 public class Auth extends Pane {
 
     public Auth() {
-        super(new InputText("Whereas recognition of the inherent dignity", "Help text", "person"));
+        /* super(new InputText("Whereas recognition of the inherent dignity", "A text", "Help text", "person")); */
+        super(new SelectorInput("Whereas recognition of the inherent dignity", "Help text", "person"));
 
 
 
