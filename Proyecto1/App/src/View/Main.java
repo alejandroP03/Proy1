1498eeee57e343a28a535b86c1fe3d535e744e22
alejandroP03 <d.pulidob@uniandes.edu.co>
2023-05-02 14:29:00 -0500

    MOdified grid object

--- Proyecto1/App/src/View/Main.java
+++ Proyecto1/App/src/View/Main.java
@@ -1,13 +1,19 @@
 package View;
 
-
-
-import View.Screens.AdminScreen.*;
-
+import View.Components.ObjectsList;
+import View.Screens.AdminScreen.CreateServiceScreen;
+import View.Screens.AdminScreen.FoodInventory;
+import View.Screens.AdminScreen.RoomInventory;
+import View.Screens.AdminScreen.RoomManaging;
+import View.Screens.AdminScreen.ServicesInventory;
 import View.Screens.AuthScreen.Auth;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,16 +26,16 @@ public class Main extends Application {
 
         Pane prb = new RoomManaging(); // Probar pestana principal Admin
         Pane inventoryRoom = new RoomInventory(); // Probar inventario habitaciones
-        Pane inventoryFood = new FoodInventory();  // Probar inventario comidas
+        Pane inventoryFood = new FoodInventory(); // Probar inventario comidas
         Pane invetoryService = new ServicesInventory(); // Probar inventario servicios
-        Pane createService = new CreateServiceScreen();  // Probar creacion servicios
+        Pane createService = new CreateServiceScreen(); // Probar creacion servicios
 
+        ObjectsList obj = new ObjectsList(new String[] { "Nombre", "Camas", "Caracteristica", "Lugar", "Tipo" });
+        obj.addElem(new Node[]{new Text("Pene"), new Label("Hello"), new Button("Jar")});
 
-        Scene scene = new Scene(createService);
-
+        Scene scene = new Scene(obj);
 
         primaryStage.setTitle("Panel con background color");
-
 
         scene.getStylesheets().add("View/Styles/font.css");
 
